249ce56dae42602f7f5daf7db24038ff76b21cfb
Miguel Jacamo <migjach@gmail.com>
2020-12-01 20:28:52 -0600

    Modificacion de metodos de logica de negocio

--- src/routes/catalogos.js
+++ src/routes/catalogos.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const {getAll,createOne} = require('../controllers/cotalogos');
+const {getAll,createOne, getOne, updateOne, deleteOne} = require('../controllers/cotalogos');
 
 router.get('/', getAll);
-router.get('/:id', (req, res)=> res.send({name: 'uno', key:req.params.id}));
+router.get('/:id', getOne);
 router.post('/', createOne);
-router.put('/:id', (req, res)=> res.send({name: 'Elimina', key: req.params.id}));
-router.delete('/:id', (req, res)=> res.send({name: 'Elimina', key: req.params.id}));
+router.put('/:id', updateOne );
+router.delete('/:id', deleteOne );
 
 module.exports = router;
